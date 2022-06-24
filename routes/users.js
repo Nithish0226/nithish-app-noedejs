7c2775258aa44ff87316defebbbe76536e9d18df
nithishkumar-docu3c <91580836+nithishkumar-docu3c@users.noreply.github.com>
2022-06-24 20:23:41 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -31,8 +31,18 @@ console.log(__filename)
 console.log("down port")
 // Url of the image
 const file = 'mynewfile1.txt';
+
+// Path at which image will get downloaded
+const filePath = `${__dirname}`;
+  
+download(file,filePath)
+.then(() => {
+    console.log('Download Completed');
+})
+
 // Path at which image will get downloaded
 res.send(`${__dirname}`+'\\mynewfile1.txt')
+
 })
 /* GET users listing. */
 router.get('/', function(req, res, next) {
